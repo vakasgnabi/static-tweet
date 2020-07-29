90f4466eaaf690154d0166e753136835014eaf4f
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-07-29 23:19:28 +0500

    static tweet based

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+
+
+const Time = ()=>(
+    <span className='time'>3h ago</span>
+    );
+
+const ReplyButton = ()=> (
+    <i className="fa fa-reply reply-button"/>
+    );
+
+const RetweetButton = () =>(
+    <i className="fa fa-retweet retweet-button" />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const LikeButton = ()=>(
+    <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = ()=>(
+    <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+function Tweet(){
+    return (
+        <div className="tweet">
+            <Avatar />
+            <div className="content">
+
+                <Author /> <Time />
+                <Message />
+                <div className="buttons">
+                    <ReplyButton/>
+                    <RetweetButton/>
+                    <LikeButton/>
+                    <MoreOptionsButton/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Avatar(){
+    return (
+        <img src="https://www.gravatar.com/avatar/nothing"
+        className="avatar"
+        alt="avatar" />
+    )
+}
+
+function Message(){
+    return(
+        <div className="message">
+            This is less than 140 characters
+        </div>
+    );
+}
+
+function Author(){
+    return(
+        <span className="author">
+            <span className="name">Your Name</span>
+            <span className="handle">@yourhandle</span>
+        </span>
+    );
+}
+
+
+ReactDOM.render(<Tweet />,
+    document.querySelector('#root'));
